5e6b6aeec0c65288093a097103afa37c229b028b
Marcos Lukac <70305649+MarcosLukac@users.noreply.github.com>
2020-08-27 08:38:28 -0300

    Add files via upload

---
+++ JPGtoPNGconverter.py
@@ -0,0 +1,22 @@
+import sys
+import os
+from PIL import Image
+
+path = sys.argv[1]
+directory = sys.argv[2]
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+def convert():
+    for filename in os.listdir(path):
+        clean_name = os.path.splitext(filename)[0]
+        img = Image.open(f'{path}/{filename}')
+        #added the / in case user doesn't enter it. You may want to check for this and add or remover it. 
+        img.save(f'{directory}\{clean_name}.png', 'png')
+    print('all done!')
+
+convert()
+#loop through Pokedex
+#convert images to png
+#save to the new folder
